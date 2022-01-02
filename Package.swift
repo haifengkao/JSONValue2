964674e4bf8e41d2cae70694667adb8d7901eda7
Hai Feng Kao <haifeng@cocoaspice.in>
2022-01-02 18:22:23 +0800

    update package.swift

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "JSONValueRX",
-            targets: ["JSONValueRX"]
+            name: "JSONValue2",
+            targets: ["JSONValue2"]
         ),
     ],
     dependencies: [
@@ -24,13 +24,13 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "JSONValueRX",
+            name: "JSONValue2",
             dependencies: [],
             path: "Sources"
         ),
         .testTarget(
             name: "JSONValueTests",
-            dependencies: ["JSONValueRX"],
+            dependencies: ["JSONValue2"],
             path: "Tests"
         ),
     ]
